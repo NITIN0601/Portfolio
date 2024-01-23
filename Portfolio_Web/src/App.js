ace59e3cfec6c9b798b0b2a6affa72822e6c302a
Nitin <dmnitin97@gmail.com>
2024-01-23 12:48:48 -0500

    Version_1.0

--- Portfolio_Web/src/App.js
+++ Portfolio_Web/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// components
+import Header from "../src/components/Header";
+import Banner from "../src/components/Banner";
+import Nav from "../src/components/Nav";
+import About from "../src/components/About";
+import Services from "../src/components/Services";
+import Work from "../src/components/Work";
+import Contact from "../src/components/Contact";
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='bg-site bg-no-repeat bg-cover overflow-hidden'>
+      <Header />
+      <Banner />
+      <Nav />
+      <About />
+      <Services />
+      <Work />
+      <Contact />
+      <div className='h-[4000px]'></div>
     </div>
   );
-}
+};
 
 export default App;
+
